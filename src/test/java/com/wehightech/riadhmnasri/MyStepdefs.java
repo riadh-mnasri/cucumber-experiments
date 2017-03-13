de00c725a982c65d5fdaed8002b45396ed7def95
Riadh MNASRI <riadh.mnasri@yahoo.fr>
2017-03-13 16:41:40 +0100

    Some enhancements

--- src/test/java/com/wehightech/riadhmnasri/MyStepdefs.java
+++ src/test/java/com/wehightech/riadhmnasri/MyStepdefs.java
@@ -1,12 +1,22 @@
 package com.wehightech.riadhmnasri;
 
-
 import cucumber.api.java8.En;
+import org.junit.Assert;
 
 public class MyStepdefs implements En {
     public MyStepdefs() {
             Given("I have (\\d+) cukes in my belly", (Integer cukes) -> {
-            System.out.format("Cukes: %n\n", cukes);
+            System.out.println("Cukes number :: "+ cukes);
         });
+        When("^I wait (\\d+) hour$", (Integer hour) -> {
+            // Write code here that turns the phrase above into concrete actions
+             System.out.println("Hour number :: "+ hour);
+        });
+
+        Then("^my belly should growl$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            Assert.assertTrue(true);
+        });
+
     }
 }
