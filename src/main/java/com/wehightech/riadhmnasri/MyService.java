d63bf14b872f08ee25c1c97d4cd39dc761bafd5d
Riadh MNASRI <riadh.mnasri@yahoo.fr>
2017-03-13 17:15:47 +0100

    Externalise service

---
+++ src/main/java/com/wehightech/riadhmnasri/MyService.java
@@ -0,0 +1,12 @@
+package com.wehightech.riadhmnasri;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyService
+{
+    public void doSomething() {
+        System.out.println("MyService.doSomething");
+    }
+}
--- src/test/java/com/wehightech/riadhmnasri/MyStepdefs.java
+++ src/test/java/com/wehightech/riadhmnasri/MyStepdefs.java
@@ -4,6 +4,7 @@ import cucumber.api.java8.En;
 import org.junit.Assert;
 
 public class MyStepdefs implements En {
+    private MyService myService = new MyService();
     public MyStepdefs() {
             Given("I have (\\d+) cukes in my belly", (Integer cukes) -> {
             System.out.println("Cukes number :: "+ cukes);
@@ -11,10 +12,12 @@ public class MyStepdefs implements En {
         When("^I wait (\\d+) hour$", (Integer hour) -> {
             // Write code here that turns the phrase above into concrete actions
              System.out.println("Hour number :: "+ hour);
+
         });
 
         Then("^my belly should growl$", () -> {
             // Write code here that turns the phrase above into concrete actions
+            myService.doSomething();
             Assert.assertTrue(true);
         });
 
